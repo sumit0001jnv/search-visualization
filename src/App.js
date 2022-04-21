323e5e47009125611bc65397677fe87e9248583f
Sumit Kumar <sumit0001.jnv@gmail.com>
2022-04-21 20:10:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NetworkGraph from "./pages/network-graphs/NetworkGraph";
+function App() {
+  return (
+    <div className="App">
+      <NetworkGraph></NetworkGraph>
+    </div>
+  );
+}
+
+export default App;
